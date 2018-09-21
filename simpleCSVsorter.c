804ab9e73ecb5ba6b662b926eefe265a205fe79f
Ian Morenot <imorenodev@gmail.com>
2018-09-21 12:41:51 -0400

    almost working implementation of dynamic allocation

--- simpleCSVsorter.c
+++ simpleCSVsorter.c
@@ -13,7 +13,7 @@ struct MovieRecord {
 };
 
 void printArguments(int argCount, char *argArray[]);
-void printMovieRecords(struct MovieRecord *arrayOfMovieRecords, int lengthOfArrayOfMovieRecords);
+void printMovieRecords(struct MovieRecord **arrayOfMovieRecords, int lengthOfArrayOfMovieRecords);
 char **getArrayOfTokens(char * stringBuffer, const char *delimiter);
 int getLine(char line[], int maxLength);
 
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
     //printArguments(argc, argv);
     
     // create array that holds 10 movieRecord pointers (10 pointers that each point to a movieRecord)
-    struct MovieRecord **arrayOfMovieRecords = malloc(sizeof(struct MovieRecord *) * 10);
+    struct MovieRecord **arrayOfMovieRecords = malloc(sizeof(struct MovieRecord *) * MAX_NUM_RECORDS);
 
     if (arrayOfMovieRecords == NULL)
     {
@@ -31,46 +31,80 @@ int main(int argc, char *argv[])
         return -1;
     }
 
+    /*
+    struct MovieRecord *pMovieA = malloc(sizeof(struct MovieRecord *));
     struct MovieRecord movieA = { "red", 10, "turtle" };
+    pMovieA = &movieA;
+    arrayOfMovieRecords[0] = pMovieA;
+
     struct MovieRecord movieB = { "green", 90, "bear" };
-    struct MovieRecord movieC = { "black", 20, "cat" };
+    arrayOfMovieRecords[1] = &movieB;
+
+    struct MovieRecord *pMovieC = malloc(sizeof(struct MovieRecord *));
+    pMovieC->color = "red";
+    pMovieC->number = 777;
+    pMovieC->animal= "BULL";
+    arrayOfMovieRecords[2] = pMovieC;
+
+    struct MovieRecord *pMovieD = malloc(sizeof(struct MovieRecord *));
     struct MovieRecord movieD = { "blue", 5, "cow" };
+    pMovieD = &movieD;
+    arrayOfMovieRecords[3] = pMovieD;
 
-    arrayOfMovieRecords[0] = movieA;
-    arrayOfMovieRecords[1] = movieB;
-    arrayOfMovieRecords[2] = movieC;
-    arrayOfMovieRecords[3] = movieD;
-
-    for (int i = 0; i < 5; i++) 
+    for (int j = 0; j < 5; j++) 
     {
-        struct MovieRecord *movie = malloc(sizeof(struct MovieRecord*));
-        movie = &(arrayOfMovieRecords[i]);
+        struct MovieRecord *pTempMovieRecord = malloc(sizeof(struct MovieRecord));
+        pTempMovieRecord->color = "red";
+        pTempMovieRecord->number= j;
+        pTempMovieRecord->animal = "bird";
+        arrayOfMovieRecords[j] = pTempMovieRecord;
+    }
+
+    for (int i = 0; i < 4; i++) 
+    {
+        struct MovieRecord *movie = arrayOfMovieRecords[i];
         printf("arrayOfMovieRecords[%d]\tcolor: %s\tnumber: %d\tanimal: %s\t\n", i, movie->color, movie->number, movie->animal); 
+        // NULL out the example MovieRecord pointers
+        arrayOfMovieRecords[i] = NULL;
     }
     */
-
-    printf("\n");
-
+ 
     // get input from csv
+    int numberOfMovieRecords = 0;
     char line[MAX_LINE_LENGTH];
-    char **arrayOfTokens = malloc(sizeof(char *)*30);
+    char **arrayOfTokens = malloc(sizeof(char *) * MAX_NUM_COLUMNS);
 
-    for (int i = 0; i < 4; i++)
+    if (arrayOfTokens == NULL)
+    {
+        fprintf(stderr, "Malloc Failed to allocate memory for arrayOfTokens");
+        return -1;
+    }
+
+    for (int i = 0; i < MAX_NUM_RECORDS; i++)
     {
         while (getLine(line, MAX_LINE_LENGTH) != EOF)
         {
             //printf("you typed \"%s\"\n", line);
             arrayOfTokens = getArrayOfTokens(line, DELIMITER);
             //printf("arrayOfTokens:\tcolor:%s\tnumber:%s\tanimal:%s\n", arrayOfTokens[0], arrayOfTokens[1], arrayOfTokens[2]);
-            
-            arrayOfMovieRecords[i].color = arrayOfTokens[0];
-            arrayOfMovieRecords[i].number = atoi(arrayOfTokens[1]);
-            arrayOfMovieRecords[i].animal = arrayOfTokens[2];
+
+            struct MovieRecord *pTempMovieRecord = malloc(sizeof(struct MovieRecord));
+            pTempMovieRecord->color = arrayOfTokens[0];
+            pTempMovieRecord->number= atoi(arrayOfTokens[1]);
+            pTempMovieRecord->animal = arrayOfTokens[2];
+            arrayOfMovieRecords[i] = pTempMovieRecord;
+
+            //printf("%s,%d,%s\n", arrayOfMovieRecords[i]->color, arrayOfMovieRecords[i]->number, arrayOfMovieRecords[i]->animal);
+            numberOfMovieRecords++;
         }
 
     }
 
-    printMovieRecords(arrayOfMovieRecords, 4);
+    for (int i = 0; i < numberOfMovieRecords; i++)
+    {
+        printf("%s,%d,%s\n", arrayOfMovieRecords[i]->color, arrayOfMovieRecords[i]->number, arrayOfMovieRecords[i]->animal);
+    }
+    //printMovieRecords(arrayOfMovieRecords, numberOfMovieRecords);
 
     return 0;
 }
@@ -107,14 +141,16 @@ char **getArrayOfTokens(char *stringBuffer, const char *delimiter)
     return arrayOfTokens;
 }
 
-void printMovieRecords(struct MovieRecord *arrayOfMovieRecords, int lengthOfArrayOfMovieRecords)
+void printMovieRecords(struct MovieRecord **arrayOfMovieRecords, int lengthOfArrayOfMovieRecords)
 {
-   for (int i = 0; i < lengthOfArrayOfMovieRecords; i++)
-   {
-        printf("%s,%d,%s\n", arrayOfMovieRecords[i].color, arrayOfMovieRecords[i].number, arrayOfMovieRecords[i].animal);
-   }
 
-   return;
+    struct MovieRecord **tempArrayOfMovieRecords = arrayOfMovieRecords;
+    for (int i = 0; i < lengthOfArrayOfMovieRecords; i++)
+    {
+        printf("%s,%d,%s\n", tempArrayOfMovieRecords[i]->color, tempArrayOfMovieRecords[i]->number, tempArrayOfMovieRecords[i]->animal);
+    }
+
+    return;
 }
 
 int getLine(char line[], int maxLength)
@@ -139,8 +175,11 @@ int getLine(char line[], int maxLength)
     }
 
     if (aChar == EOF && (index == 0))
+    {
         return EOF;
+    }
 
     line[index] = '\0';
+
     return index;
 }
--- simpleCSVsorter.h
+++ simpleCSVsorter.h
@@ -11,6 +11,8 @@
 static const char *DEBUGGING = "**DEBUGGING**\t";
 static const char *DELIMITER= ",";
 static const int MAX_LINE_LENGTH = 512;
+static const int MAX_NUM_RECORDS = 5500;
+static const int MAX_NUM_COLUMNS = 30;
 
 //Suggestion: define a struct that mirrors a record (row) of the data set
 
